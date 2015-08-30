e15c5e578d652ed2de7487763c583af6b1ac396a
Vlad Soroka <volkzayaz@gmail.com>
2015-08-30 21:03:54 +0300

    5 squares

--- Squares/Squares/ViewController.swift
+++ Squares/Squares/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textField: UITextField!
+   
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                super.viewDidLoad()
+        let a = UIView(frame: CGRectMake(0, 0, 320, 480))
+        a.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(a)
+        let b = UIView(frame: CGRectMake(20, 20, 280, 440))
+        b.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(b)
+        let c = UIView(frame: CGRectMake(40, 40, 240, 400))
+        c.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(c)
+        let d = UIView(frame: CGRectMake(60, 60, 200, 360))
+        d.backgroundColor = UIColor.redColor()
+        self.view.addSubview(d)
+        let e = UIView(frame: CGRectMake(80, 80, 160, 320))
+        e.backgroundColor = UIColor.purpleColor()
+        self.view.addSubview(e)
+       
+                // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
 
 }
 
