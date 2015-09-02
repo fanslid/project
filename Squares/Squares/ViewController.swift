8a517be456383b91360656c0dc3b05af13de1477
Vlad Soroka <volkzayaz@gmail.com>
2015-09-02 17:52:20 +0300

    5 squares with loop

--- Squares/Squares/ViewController.swift
+++ Squares/Squares/ViewController.swift
@@ -16,21 +16,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
                 super.viewDidLoad()
-        let a = UIView(frame: CGRectMake(5, 5, 310, 310))
-        a.backgroundColor = UIColor.yellowColor()
-        self.view.addSubview(a)
-        let b = UIView(frame: CGRectMake(10, 10, 300, 300))
-        b.backgroundColor = UIColor.blueColor()
-        self.view.addSubview(b)
-        let c = UIView(frame: CGRectMake(15, 15, 290, 290))
-        c.backgroundColor = UIColor.blackColor()
+        let n = 5
+        //square : UIView(frame: CGRectMake(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat))
+        for var i=1; i<=n; ++i {
+            var square = UIView(frame: CGRectMake(CGFloat(5 * i), CGFloat(5 * i), CGFloat(320 - 10 * i), CGFloat(320 - 10 * i)))
+            square.backgroundColor = UIColor(red: (0.1),green: (0.1), blue: (0.1), alpha: (0.1))
+            self.view.addSubview(square)
+        }
+        
+        /* let a = UIView(frame: CGRectMake(5, 5, 310, 310))
+       a.backgroundColor = UIColor.yellowColor()
+       self.view.addSubview(a)
+       let b = UIView(frame: CGRectMake(10, 10, 300, 300))
+       b.backgroundColor = UIColor.blueColor()
+       self.view.addSubview(b)
+       let c = UIView(frame: CGRectMake(15, 15, 290, 290))
+      c.backgroundColor = UIColor.blackColor()
         self.view.addSubview(c)
         let d = UIView(frame: CGRectMake(20, 20, 280, 280))
-        d.backgroundColor = UIColor.redColor()
-        self.view.addSubview(d)
+       d.backgroundColor = UIColor.redColor()
+       self.view.addSubview(d)
         let e = UIView(frame: CGRectMake(25, 25, 270, 270))
         e.backgroundColor = UIColor.purpleColor()
-        self.view.addSubview(e)
+        self.view.addSubview(e)*/
        
                 // Do any additional setup after loading the view, typically from a nib.
     }
@@ -38,9 +46,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        }
+
     }
 
-   
 
-}
 
